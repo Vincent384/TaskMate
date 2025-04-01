3cd4097b0fcfad99c8ddf49f056bce039c278c4e
Vincent <vincentmu3@hotmail.com>
2025-04-01 09:49:26 +0200

    Skapar CreatePage

--- app/components/CreatePage.jsx
+++ app/components/CreatePage.jsx
@@ -5,8 +5,10 @@ const CreatePage = () => {
 
   const [form, setForm] = useState({
     title:'',
+    work:''
   })
 
+  const [formArray, setFormArray] = useState([])
 
   function onChangeHandler(e){
     const {name,value} = e.target
@@ -19,19 +21,70 @@ const CreatePage = () => {
     })
   }
 
+  console.log(formArray)
+
+  function onSubmitHandler(e){
+    e.preventDefault()
+
+    setFormArray((prev) => {
+      return [
+        ...prev,
+        {
+          work:form.work
+        }
+      ]
+    })
+
+    setForm((prev) => {
+      return {
+        ...prev,
+        work:''
+      }
+    })
+  }
+
   return (
     <div className='flex justify-center items-center'>
-      <div className='bg-amber-50 p-10 my-10'>
+      <div className='bg-amber-50 p-10 my-10 rounded-lg'>
 
-      <form className=''>
+      <form onSubmit={e => onSubmitHandler(e)} className=''>
         <Input
          placeholderText={'Min titel...'} 
          labelText={'Titel'} 
          inputType={'text'}
          valueInput={form.title}
          onChangeHandler={onChangeHandler}
-         nameInput={'title'}  />
+         nameInput={'title'} 
+         textArea={false} />
+        <Input
+         placeholderText={'Min uppgift är...'} 
+         labelText={'Din Uppgift'} 
+         inputType={'text'}
+         valueInput={form.work}
+         onChangeHandler={onChangeHandler}
+         nameInput={'work'} 
+         textArea={true} />
+         <button className='w-full py-2 bg-emerald-600 cursor-pointer text-white font-semibold mt-5 rounded-lg
+        transision-all hover:bg-emerald-800'>Lägg till</button>
       </form>
+      {
+        form.title !== '' ? 
+        <div className='flex flex-col gap-3 mt-5 font-semibold text-center'>
+          <span className='border-b-2'>
+            Titel
+          </span>
+          <span className='py-2 px-4 bg-slate-300 text-center rounded-lg w-full mt-2'>
+          {form.title}
+          </span>
+          </div> :''
+      }
+      {
+      formArray && formArray.length > 0 ? formArray.map((item,index) => (
+          <div key={index}>
+            <p className='py-2 px-4 font-semibold bg-amber-400 rounded-lg w-full mt-3 '>{item.work}</p>
+          </div>
+        )) :''
+      }
         </div>
     </div>
   )
--- app/components/input.jsx
+++ app/components/input.jsx
@@ -1,14 +1,36 @@
 import React from 'react'
 
-export const Input = ({placeholderText,labelText,inputType,valueInput,onChangeHandler,nameInput}) => {
+export const Input = ({placeholderText,
+  labelText,
+  inputType,
+  valueInput,
+  onChangeHandler,
+  nameInput,
+  textArea}) => {
   return (
-    <div className='flex flex-col gap-3 mt-5'>
+    <>
+    { textArea ? (
+      <div className='flex flex-col gap-3 mt-5'>
+       
+        <label className='text-lg' htmlFor="">{labelText}</label>
+        <textarea className='border rounded-lg resize-none min-h-[100px]'
+        name={nameInput}
+        value={valueInput} onChange={e => onChangeHandler(e)}
+        placeholder={placeholderText} id=""></textarea>
+    </div>
+    ) :
+    (
+      <div className='flex flex-col gap-3 mt-5'>
+       
         <label className='text-lg' htmlFor="">{labelText}</label>
         <input className='border pr-10 pl-2 py-2 rounded-lg' 
         type={inputType} value={valueInput}
         name={nameInput}
-         onChange={(e) =>onChangeHandler(e)}
-         placeholder={placeholderText}/>
+        onChange={(e) =>onChangeHandler(e)}
+        placeholder={placeholderText}/>
     </div>
+    )  
+      }
+  </>
   )
 }
